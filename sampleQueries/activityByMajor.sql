58a6f76fc05cbf54841768c1736f834de018eb38
Dong <rdong6@fastx07.divms.uiowa.edu>
2017-11-24 17:09:34 -0600

    reality check for querying most needed majors

---
+++ sampleQueries/activityByMajor.sql
@@ -0,0 +1,37 @@
+SELECT major, count(*) AS questionsSolved
+FROM 
+	(
+		(SELECT major
+		FROM 
+			(SELECT tutorID 
+			FROM questionAnsweredBy t1
+			) nj1
+			NATURAL JOIN
+			(SELECT tutorID, major 
+			FROM tutor t2
+			) nj2
+		)
+		UNION ALL
+		(SELECT major
+		FROM 
+			(SELECT tutorID 
+			FROM expertAnswerGivenBy t3
+			) nj3
+			NATURAL JOIN 
+			(SELECT tutorID, major 
+			FROM tutor t4
+			) nj4
+		)
+		UNION ALL
+		(SELECT major
+		FROM 
+			(SELECT tutorID 
+			FROM textbookSolutionGivenBy t5
+			) nj5
+			NATURAL JOIN 
+			(SELECT tutorID, major 
+			FROM tutor
+			) nj6
+		)
+	) u
+GROUP BY major
