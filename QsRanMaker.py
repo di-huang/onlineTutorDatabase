9360ecb7d2f20a6ac04897d766f76e2c698348e5
Di Huang <di-huang@uiowa.edu>
2017-11-24 15:39:04 -0600

    insert Qs|make Qs randomly|automake sql files
    InsertQuestions.sql: insert 500x questions
    QsRanMaker.py: make questions randomly (need to import sqlfileMaker and install&import pymysql)
    sqlfileMaker.py: make sql files automatically

---
+++ QsRanMaker.py
@@ -0,0 +1,66 @@
+import pymysql
+import random
+from sqlfileMaker import *
+
+QsSubjs = ['Algebra', 'Geometry', 'Trigonometry', 'Calculus', 'Advanced Math',	# Math
+			'Physics', 'Chemistry', 'Biology', 'Earth Science', 'Nursing',		# Science
+			'Computer Science', 'Electrical Engineering', 'Chemical Engineering', # Engineering
+			'Finance', 'Accounting', 'Economics', 'Operations Management']		# Business
+
+# change to your db info if you want to test
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd='root', db='db_dhuang' )
+cursor = db.cursor()
+
+# set autocommit off to be able to rollback your commit
+# cursor.execute('set autocommit = 0')
+db.autocommit(False)
+
+# fetch all students from student table
+cursor.execute('select studentID from student')
+res_fetch_stu = cursor.fetchall()
+if res_fetch_stu == None or len(res_fetch_stu) == 0:
+	exit()
+
+''' 
+insert questions randomly
+if make = True, then execute insert cmds and make sql files 
+else then only make sql files
+'''
+def exe_makefile(exe):
+	N = int(input())			# the num of Qs to be inserted
+	iniN = 100000				# initial question id number to start (default: 100000)
+	sql = 'select questionID from questions order by questionid desc limit 1'
+	cursor.execute(sql)
+	res = cursor.fetchone()
+	if res != None and len(res) != 0:
+		iniN = int(res[0])+1
+
+	fm = fileMaker('InsertQuestions.sql')
+	fm.makehead('questions')
+
+	# question format: qid, sid, qsubj, qcont
+	for qid in range(iniN, iniN+N):
+		sid = random.choice(res_fetch_stu)[0]
+		qsubj = random.choice(QsSubjs)
+		if exe:
+			sql = ("insert into questions values "
+				"('%s', '%s', '%s', '%s')") % (str(qid), sid, qsubj, 'question content')
+			cursor.execute(sql)
+		fm.makebody(('%s', '%s', '%s', '%s'), (str(qid), sid, qsubj, 'question content'))
+
+	fm.makeend()
+	if exe:
+		db.commit()
+		# rollback if you want
+		#cursor.execute('rollback')
+
+# only make sql files
+exe_makefile(False)
+# execute insert cmds and make sql files 
+# exe_makefile(True)
+
+
+
+
+
+
