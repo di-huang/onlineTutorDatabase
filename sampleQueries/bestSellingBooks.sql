2ca2e5aba5fd10d7fff46a0742052b06b0d9a9c1
Dong <rdong6@fastx07.divms.uiowa.edu>
2017-11-24 21:05:11 -0600

    bestSellingBook work in progress

--- sampleQueries/bestSellingBooks.sql
+++ sampleQueries/bestSellingBooks.sql
@@ -1,17 +1,21 @@
-(SELECT ISBN, sum(quantity) AS copiesSold, price AS unitPrice, copiesSold * unitPrice AS totalIncome
-FROM suppliedBy
-WHERE
-	(description = "purchase")
-	AND
-	(totalIncome >= all
-		(SELECT sum(quantity) * price AS totalIncome2
+SELECT ISBN, copiesSold, unitPrice, totalIncome
+FROM
+	(
+		(SELECT ISBN, sum(quantity) AS copiesSold, price AS unitPrice, copiesSold * unitPrice AS totalIncome
 		FROM suppliedBy
+		WHERE
+			(description = "purchase")
+			AND
+			(totalIncome >= all
+				(SELECT sum(quantity) * price AS totalIncome2
+				FROM suppliedBy
+				GROUP BY ISBN
+				)
+			)
 		GROUP BY ISBN
-		)
+		) nj1
+		NATURAL JOIN
+		(SELECT ISBN, bookName, author
+		FROM book
+		) nj2
 	)
-GROUP BY ISBN
-)
-NATURAL JOIN
-(SELECT ISBN, bookName, author
-FROM book
-)
