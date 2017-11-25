23dc32c432b1eee76b9092d5a882593d7579ebb7
Dong <rdong6@fastx07.divms.uiowa.edu>
2017-11-24 22:39:42 -0600

    bestSellingBooks working with various prices of same ISBN

--- sampleQueries/bestSellingBooks.sql
+++ sampleQueries/bestSellingBooks.sql
@@ -1,17 +1,24 @@
-SELECT ISBN, bookName, author, copiesSold, unitPrice, totalIncome
+SELECT ISBN, bookName, author, copiesSold, averagePrice, totalIncome
 FROM
 	(
-		(SELECT ISBN, copiesSold, unitPrice, totalIncome
+		(SELECT ISBN, copiesSold, averagePrice, totalIncome
 		FROM
-	       		(SELECT ISBN, sum(quantity) AS copiesSold, price AS unitPrice, sum(quantity) * price AS totalIncome
-			FROM suppliedBy
-			WHERE description = "purchase"
+	       		(SELECT ISBN, SUM(quantity) AS copiesSold, AVG(price) AS averagePrice, SUM(total) AS totalIncome
+			FROM 
+				(SELECT ISBN, quantity, price, price * quantity AS total
+				FROM suppliedBy
+				WHERE description = "purchase"
+				) t1
 			GROUP BY ISBN
-			) t1	
+			) t2
 		WHERE
 			(totalIncome >= all
-				(SELECT sum(quantity) * price
-				FROM suppliedBy
+				(SELECT SUM(total)
+				FROM
+					(SELECT ISBN, price * quantity AS total
+					FROM suppliedBy
+					WHERE description = "purchase"
+					) t3
 				GROUP BY ISBN
 				)
 			)
