cf5e6d3d71595e61fe63b480c104773ff6df47ef
Dong <rdong6@serv16.divms.uiowa.edu>
2017-12-03 13:02:40 -0600

    simplified acitivityByMajor.sql

--- sampleQueries/activityByMajor.sql
+++ sampleQueries/activityByMajor.sql
@@ -1,37 +1,26 @@
 SELECT major, count(*) AS questionsSolved
 FROM 
-	(
-		(SELECT major
-		FROM 
+	(SELECT major
+	FROM 
+		(
 			(SELECT tutorID 
-			FROM questionAnsweredBy t1
-			) nj1
-			NATURAL JOIN
-			(SELECT tutorID, major 
-			FROM tutor t2
-			) nj2
-		)
-		UNION ALL
-		(SELECT major
-		FROM 
-			(SELECT tutorID 
-			FROM expertAnswerGivenBy t3
-			) nj3
-			NATURAL JOIN 
-			(SELECT tutorID, major 
-			FROM tutor t4
-			) nj4
-		)
-		UNION ALL
-		(SELECT major
-		FROM 
-			(SELECT tutorID 
-			FROM textbookSolutionGivenBy t5
-			) nj5
-			NATURAL JOIN 
-			(SELECT tutorID, major 
-			FROM tutor
-			) nj6
-		)
-	) u
+			FROM expertAnswerGivenBy
+			)
+			UNION ALL
+			(SELECT tutorID
+			FROM textbookSolutionGivenBy
+			)
+			UNION ALL
+			(SELECT tutorID
+			FROM questionAnsweredBy
+			)
+		) nj1
+		NATURAL JOIN
+		(SELECT tutorID, major
+		FROM tutor
+		) nj2
+	) t1
 GROUP BY major
+;
+			
+			
